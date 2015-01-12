5f06ba7bd4c6f37e43fc3dbd03abce49e6908a1b
MattiaEvent <MattiaEvent@WIN-V5B5FNL0AI7.localdomain>
2015-01-12 15:54:55 +0100

    -aaa

--- AmIDrools/src/ami_drools/RuleRunner.java
+++ AmIDrools/src/ami_drools/RuleRunner.java
@@ -228,7 +228,6 @@ public class RuleRunner {
 				+ "end \n";
 		s="";
 			try {
-				//BufferedReader br = new BufferedReader(new FileReader("C:/Users/MattiaEvent/workspace/AmIDrools/drl.txt"));
 				BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/resources/drl.txt"));
 				StringBuilder sb = new StringBuilder();
 		        String line = br.readLine();
