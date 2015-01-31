9e23140c8e0857578c3e54407ced6488bab91f7c
pietro29 <pietro.care@gmail.com>
2015-01-31 14:41:34 +0100

    Correzione errore su stringa

--- AmIDrools/src/sharedFacts/Lampadina.java
+++ AmIDrools/src/sharedFacts/Lampadina.java
@@ -54,12 +54,12 @@ public class Lampadina implements java.io.Serializable{
     	 switch (field){
     	 case "codice" : setCodice(value);
     	 				break;
-    	 case "accesa" : if (value=="true")
+    	 case "accesa" : if (value.equals("true"))
     		 				setAccesa(true);
     	 				else
     	 					setAccesa(false);
     	 				break;
-    	 case "spenta" : if (value=="true")
+    	 case "spenta" : if (value.equals("true"))
 				setSpenta(true);
 			else
 				setSpenta(false);
